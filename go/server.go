a9e3d919205f05e1928760350ad9046dd44b0ed2
Jason Martens <jmartens@zendesk.com>
2019-08-21 15:56:06 -0700

    Run gofmt on server

--- go/server.go
+++ go/server.go
@@ -1,40 +1,40 @@
 package main
 
 import (
+	gintrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/gin-gonic/gin"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 	"net"
 	"os"
-    gintrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/gin-gonic/gin"
-    "gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
-    addr := net.JoinHostPort(
-        os.Getenv("DD_AGENT_HOST"),
-        os.Getenv("DD_TRACE_AGENT_PORT"),
-    )
+	addr := net.JoinHostPort(
+		os.Getenv("DD_AGENT_HOST"),
+		os.Getenv("DD_TRACE_AGENT_PORT"),
+	)
 
-    tracer.Start(tracer.WithAgentAddr(addr))
-    defer tracer.Stop()
+	tracer.Start(tracer.WithAgentAddr(addr))
+	defer tracer.Stop()
 
-    // Create a gin.Engine
-    r := gin.Default()
+	// Create a gin.Engine
+	r := gin.Default()
 
-    // Use the tracer middleware with your desired service name.
-    r.Use(gintrace.Middleware("go-service"))
+	// Use the tracer middleware with your desired service name.
+	r.Use(gintrace.Middleware("go-service"))
 
-    // Continue using the router as normal.
-    r.GET("/go/hello", func(c *gin.Context) {
-        c.String(200, "Hello World!")
-    })
+	// Continue using the router as normal.
+	r.GET("/go/hello", func(c *gin.Context) {
+		c.String(200, "Hello World!")
+	})
 
-    r.GET("/go/ping", func(c *gin.Context) {
+	r.GET("/go/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 
-    r.Run(":9001")
+	r.Run(":9001")
 }
